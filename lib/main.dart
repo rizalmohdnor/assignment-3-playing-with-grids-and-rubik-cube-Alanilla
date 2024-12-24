25109fc7b0a3068efb75bf62a00bf3c3fef22577
Alanilla <alanillaiium@gmail.com>
2024-12-24 13:53:27 +0800

    This is the submitted assignment completed.

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,14 @@ class MyApp extends StatelessWidget {
       title: '2x2 Rubik\'s Cube',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-      ),
+      ), //ThemeData
       home: const CubeScreen(),
-    );
+    ); //MaterialApp
   }
 }
 
 class CubeState {
-  List<List<Color>> faces = [
+  List<List<Color>> initialFaces = [
     [Colors.red, Colors.red, Colors.red, Colors.red], // Front
     [Colors.blue, Colors.blue, Colors.blue, Colors.blue], // Left
     [Colors.green, Colors.green, Colors.green, Colors.green], // Right
@@ -29,28 +29,105 @@ class CubeState {
     [Colors.white, Colors.white, Colors.white, Colors.white], // Bottom
   ];
 
-  // Rotate top face to the left, modify if necessary
-  void rotateTop() {
-    // Store the top row of each face
+  List<List<Color>> faces = [];
+
+  CubeState() {
+    resetFaces();
+  }
+
+  void resetFaces() {
+    faces = initialFaces.map((face) => [...face]).toList();
+  }
+
+  // Rotate top face to the left
+void rotateTop() {
+    // Store current rows of adjacent faces
     List<Color> topRowFront = [faces[0][0], faces[0][1]];
     List<Color> topRowLeft = [faces[1][0], faces[1][1]];
     List<Color> topRowRight = [faces[2][0], faces[2][1]];
     List<Color> topRowBack = [faces[3][0], faces[3][1]];
 
-    // Rotate top face
+    // Rotate the top face (faces[4])
     List<Color> tempTop = [...faces[4]];
-    faces[4] = [tempTop[2], tempTop[3], tempTop[0], tempTop[1]];
+    faces[4] = [
+      tempTop[2], // Move bottom-left to top-left
+      tempTop[0], // Move top-left to top-right
+      tempTop[3], // Move bottom-right to bottom-left
+      tempTop[1], // Move top-right to bottom-right
+    ];
 
     // Update adjacent faces
-    faces[0] = [faces[3][2], faces[3][3], ...faces[0].sublist(2)];
-    faces[1] = [faces[0][2], faces[0][3], ...faces[1].sublist(2)];
-    faces[2] = [faces[1][2], faces[1][3], ...faces[2].sublist(2)];
-    faces[3] = [faces[2][2], faces[2][3], ...faces[3].sublist(2)];
+    faces[0][0] = topRowRight[0];
+    faces[0][1] = topRowRight[1];
+    faces[2][0] = topRowBack[0];
+    faces[2][1] = topRowBack[1];
+    faces[3][0] = topRowLeft[0];
+    faces[3][1] = topRowLeft[1];
+    faces[1][0] = topRowFront[0];
+    faces[1][1] = topRowFront[1];
   }
-  
-  // Rotate top face to the left, modify if necessary
+
+  // Rotate bottom face to the left
   void rotateBottom() {
-    //Need to implement your code here
+    List<Color> bottomRowFront = [faces[0][2], faces[0][3]];
+    List<Color> bottomRowLeft = [faces[1][2], faces[1][3]];
+    List<Color> bottomRowRight = [faces[2][2], faces[2][3]];
+    List<Color> bottomRowBack = [faces[3][2], faces[3][3]];
+
+    //rotate the bottom face
+    List<Color> tempBottom = [...faces[5]];
+    faces[5] = [
+      tempBottom[2], // Move bottom-left to top-left
+      tempBottom[0], // Move top-left to top-right
+      tempBottom[3], // Move bottom-right to bottom-left
+      tempBottom[1], // Move top-right to bottom-right
+    ];
+
+    faces[0][2] = bottomRowLeft[0];
+    faces[0][3] = bottomRowLeft[1];
+    faces[1][2] = bottomRowBack[0];
+    faces[1][3] = bottomRowBack[1];
+    faces[2][2] = bottomRowFront[0];
+    faces[2][3] = bottomRowFront[1];
+    faces[3][2] = bottomRowRight[0];
+    faces[3][3] = bottomRowRight[1];
+  }
+
+  void rotateLeft() {
+    List<Color> leftColFront = [faces[0][0], faces[0][2]];
+    List<Color> leftColTop = [faces[4][0], faces[4][2]];
+    List<Color> leftColBottom = [faces[5][0], faces[5][2]];
+    List<Color> leftColBack = [faces[3][0], faces[3][2]];
+    List<Color> tempLeft = [...faces[1]];
+    faces[1] = [tempLeft[2], tempLeft[0], tempLeft[3], tempLeft[1]];
+
+    faces[0][0] = leftColTop[0];
+    faces[0][2] = leftColTop[1];
+    faces[4][0] = leftColBack[0];
+    faces[4][2] = leftColBack[1];
+    faces[3][0] = leftColBottom[0];
+    faces[3][2] = leftColBottom[1];
+    faces[5][0] = leftColFront[0];
+    faces[5][2] = leftColFront[1];
+  }
+
+  void rotateRight() {
+    List<Color> rightColFront = [faces[0][1], faces[0][3]];
+    List<Color> rightColTop = [faces[4][1], faces[4][3]];
+    List<Color> rightColBottom = [faces[5][1], faces[5][3]];
+    List<Color> rightColBack = [faces[3][0], faces[3][2]];
+
+    List<Color> tempRight = [...faces[2]];
+    faces[2] = [tempRight[2], tempRight[0], tempRight[3], tempRight[1]];
+
+    faces[0][1] = rightColTop[0];
+    faces[0][3] = rightColTop[1];
+    faces[4][1] = rightColBack[1];
+    faces[4][3] = rightColBack[0];
+    faces[3][0] = rightColBottom[1];
+    faces[3][2] = rightColBottom[0];
+    faces[5][1] = rightColFront[0];
+    faces[5][3] = rightColFront[1];
   }
 }
 
@@ -67,6 +144,30 @@ class _CubeScreenState extends State<CubeScreen> {
   void rotateTop() {
     setState(() {
       cube.rotateTop();
+    });
+  }
+
+  void rotateBottom() {
+    setState(() {
+      cube.rotateBottom();
+    });
+  }
+
+  void rotateLeft() {
+    setState(() {
+      cube.rotateLeft();
+    });
+  }
+
+  void rotateRight() {
+    setState(() {
+      cube.rotateRight();
+    });
+  }
+
+  void resetCube() {
+    setState(() {
+      cube.resetFaces();
     });
   }
 
@@ -88,69 +189,108 @@ class _CubeScreenState extends State<CubeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('2x2 Rubik\'s Cube'),
-        // instead of using an icon button here, create atleast 2 buttons to rotate the faces, rotate left face, or rotate right face, or implement all rotations.
         actions: [
           IconButton(
-            icon: const Icon(Icons.rotate_left),
-            onPressed: rotateTop,
-          )
+            icon: const Icon(Icons.refresh),
+            onPressed: resetCube,
+            tooltip: 'Reset Cube',
+          ),
         ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Top face
-            Column(
-              children: [
-                const Text('Top'),
-                SizedBox(
-                  height: 100,
-                  width: 100,
-                  child: buildFace(cube.faces[4]),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Left face
-                Column(
-                  children: [
-                    const Text('Left'),
-                    SizedBox(
-                      height: 100,
-                      width: 100,
-                      child: buildFace(cube.faces[1]),
-                    ),
-                  ],
-                ),
-                // Front face
-                SizedBox(
-                  height: 100,
-                  width: 100,
-                  child: buildFace(cube.faces[0]),
-                ),
-                // Right face
-                Column(
-                  children: [
-                    const Text('Right'),
-                    SizedBox(
-                      height: 100,
-                      width: 100,
-                      child: buildFace(cube.faces[2]),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            // Bottom face , implement your bottom face
-            // Rear face, implement your rear face
-                ),
-              ],
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              const Text('Top'),
+              SizedBox(
+                height: 100,
+                width: 100,
+                child: buildFace(cube.faces[4]),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  const Text('Left'),
+                  SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: buildFace(cube.faces[1]),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 100,
+                width: 100,
+                child: buildFace(cube.faces[0]),
+              ),
+              Column(
+                children: [
+                  const Text('Right'),
+                  SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: buildFace(cube.faces[2]),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              const Text('Bottom'),
+              SizedBox(
+                height: 100,
+                width: 100,
+                child: buildFace(cube.faces[5]),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              const Text('Back'),
+              SizedBox(
+                height: 100,
+                width: 100,
+                child: buildFace(cube.faces[3]),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: rotateTop,
+                    child: const Text('Rotate Top'),
+                  ),
+                  ElevatedButton(
+                    onPressed: rotateBottom,
+                    child: const Text('Rotate Bottom'),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: rotateLeft,
+                    child: const Text('Rotate Left'),
+                  ),
+                  ElevatedButton(
+                    onPressed: rotateRight,
+                    child: const Text('Rotate Right'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
